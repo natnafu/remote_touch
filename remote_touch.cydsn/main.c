a431b202b5bb04457a332338d47107b936bcc777
Nat Atnafu <natnafu@gmail.com>
2020-12-02 18:42:53 -0500

    Add serial comms, rename RTD1 to RTD0, cal RTD

--- remote_touch.cydsn/main.c
+++ remote_touch.cydsn/main.c
@@ -9,6 +9,8 @@
  *
  * ========================================
 */
+#include <stdio.h>
+
 #include "project.h"
 
 // RTD resistor divider
@@ -16,7 +18,7 @@
 #define R_TOP       1000.0    // RTD, 1kOhm at 0C
 #define R_BOT       10000.0   // descrete resistor
 #define OHM_PER_C   3.85      // RTD ohms per degree C
-#define RTD_OFFSET  30.0
+#define RTD_OFFSET  47.0
 
 float rtd_volt_to_temp(float voltage) {
     return (V_S * R_BOT - voltage * (R_TOP + R_BOT)) / (voltage * OHM_PER_C) + RTD_OFFSET;
@@ -34,12 +36,20 @@ int main(void)
     ADC_TEMP_StartConvert();
     float temp_copper;
 
+    // Serial debugging
+    UART_PC_Start();
+
     for(;;) {
 
         if (ADC_TEMP_IsEndConversion(ADC_TEMP_RETURN_STATUS)) {
-            uint32_t adc_RTD1 = ADC_TEMP_GetResult32();
-            temp_copper = rtd_volt_to_temp(ADC_TEMP_CountsTo_Volts(adc_RTD1));
+            uint32_t adc_RTD0 = ADC_TEMP_GetResult32();
+            temp_copper = rtd_volt_to_temp(ADC_TEMP_CountsTo_Volts(adc_RTD0));
         }
+
+        // Print out temperatures
+        char buf[64];
+        sprintf(buf, "RTD %i\n", (int) temp_copper);
+        UART_PC_PutString(buf);
     }
 }
 
