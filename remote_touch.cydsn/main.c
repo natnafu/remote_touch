45622fb5ed89b2b8b30d1b511564b0a6baa9158d
Nat Atnafu <natnafu@gmail.com>
2020-12-01 19:49:01 -0500

    Add heater PWMs and temp ADC

--- remote_touch.cydsn/main.c
+++ remote_touch.cydsn/main.c
@@ -1,25 +1,45 @@
 /* ========================================
  *
- * Copyright YOUR COMPANY, THE YEAR
+ * Copyright Nafu Studios, 2020
  * All Rights Reserved
  * UNPUBLISHED, LICENSED SOFTWARE.
  *
  * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * WHICH IS THE PROPERTY OF NAFU STUDIOS.
  *
  * ========================================
 */
 #include "project.h"
 
+// RTD resistor divider
+#define V_S         5.0       // 5V
+#define R_TOP       1000.0    // RTD, 1kOhm at 0C
+#define R_BOT       10000.0   // descrete resistor
+#define OHM_PER_C   3.85      // RTD ohms per degree C
+#define RTD_OFFSET  30.0
+
+float rtd_volt_to_temp(float voltage) {
+    return (V_S * R_BOT - voltage * (R_TOP + R_BOT)) / (voltage * OHM_PER_C) + RTD_OFFSET;
+}
+
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    CyGlobalIntEnable;
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    // Copper heater
+    PWM_HEATER_Start();
 
-    for(;;)
-    {
-        /* Place your application code here. */
+    // Copper temperature sensor
+    ADC_TEMP_Start();
+    ADC_TEMP_StartConvert();
+    float temp_copper;
+
+    for(;;) {
+
+        if (ADC_TEMP_IsEndConversion(ADC_TEMP_RETURN_STATUS)) {
+            uint32_t adc_RTD1 = ADC_TEMP_GetResult32();
+            temp_copper = rtd_volt_to_temp(ADC_TEMP_CountsTo_Volts(adc_RTD1));
+        }
     }
 }
 
