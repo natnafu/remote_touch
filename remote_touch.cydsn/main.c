b53a120ce957b9f1bc93ff8ec1dd894f8e179142
Nat Atnafu <natnafu@gmail.com>
2020-12-05 20:09:07 -0500

    Use external pin for temperature control
    NodeMCU sends touch states between two sets of frames via Blynk.
    Each frame controls temp based on the touch state of the other.

--- remote_touch.cydsn/main.c
+++ remote_touch.cydsn/main.c
@@ -131,10 +131,10 @@ int main(void)
             CapSense_CSD_UpdateEnabledBaselines();
             CapSense_CSD_ScanEnabledWidgets();
             is_touched = CapSense_CSD_CheckIsWidgetActive(CapSense_CSD_TOUCH0__BTN);
-            PIN_GPIO_Write(is_touched);
+            PIN_IS_TOUCHED_Write(is_touched);
         }
 
-        if (is_touched) heater_controller(temp_copper);
+        if (PIN_REMOTE_TOUCH_Read()) heater_controller(temp_copper);
         else cooling_controller();
     }
 }
