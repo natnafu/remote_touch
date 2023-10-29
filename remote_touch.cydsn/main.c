5ffa274bc548074fa46224476b9bf5f1b6c4cf8a
Nat Atnafu <natnafu@gmail.com>
2023-10-29 12:44:38 -0400

    Improve status message, increase cooling time, verify RTD offset

--- remote_touch.cydsn/main.c
+++ remote_touch.cydsn/main.c
@@ -5,6 +5,20 @@
 // Uncomment to communicate with Tuner GUI
 //#define EZTUNE
 
+// for status messages
+#define STATE_IDLE  0
+#define STATE_HEAT  1
+#define STATE_COOL  2
+#define NUM_STATES  3
+
+const char* state_names[NUM_STATES] = {
+    [STATE_IDLE] = "idle...",
+    [STATE_HEAT] = "heating",
+    [STATE_COOL] = "cooling"
+};
+
+uint32_t state = STATE_IDLE;
+
 // Get current Timer count
 uint32_t stopwatch_start(void) {
     return Timer_ReadCounter();
@@ -19,7 +33,7 @@ uint32_t stopwatch_elapsed_ms(uint32_t timer_count) {
 }
 
 // Cooling timer
-#define MAX_COOLING_TIME_MS     7000
+#define MAX_COOLING_TIME_MS     60000
 uint32_t timer_cooling = MAX_COOLING_TIME_MS;
 
 // RTD resistor divider
@@ -27,8 +41,8 @@ uint32_t timer_cooling = MAX_COOLING_TIME_MS;
 #define R_TOP       1000.0    // RTD, 1kOhm at 0C
 #define R_BOT       10000.0   // descrete resistor
 #define OHM_PER_C   3.85      // RTD ohms per degree C
-#define RTD_OFFSET  47.0      // Frame 1
-//#define RTD_OFFSET  32.0      // Frame 2
+#define RTD_OFFSET  47.0      // Frame Client
+//#define RTD_OFFSET  32.0      // Frame Server
 
 // Convert RTD voltage to temp (units C)
 float rtd_volt_to_temp(float voltage) {
@@ -45,6 +59,8 @@ float rtd_volt_to_temp(float voltage) {
 
 // Controls heating (heats up fast and then keeps steady)
 void heater_controller(float temp) {
+    state = STATE_HEAT;
+
     // Make sure peltiers are inactive (relays set to same state)
     if (!PIN_P_COOL_Read() != !PIN_P_HEAT_Read()) {
         PIN_P_COOL_Write(0);
@@ -64,14 +80,8 @@ void heater_controller(float temp) {
 
     if (temp < TARGET_TEMP) {
         PWM_HEATER_WriteCompare(UINT8_MAX);
-        char buf[64];
-        sprintf(buf, "MAX duty\n");
-        UART_PC_PutString(buf);
     } else {
         PWM_HEATER_WriteCompare(MIN_HEATER_DUTY);
-        char buf[64];
-        sprintf(buf, "min duty\n");
-        UART_PC_PutString(buf);
     }
 
     timer_cooling = stopwatch_start();  // reset cooling timer
@@ -93,11 +103,10 @@ void cooling_controller() {
 
     // Cool for a set duration to avoid overheating heatsink
     if (stopwatch_elapsed_ms(timer_cooling) < MAX_COOLING_TIME_MS) {
+        state = STATE_COOL;
         PIN_P_COOL_Write(1);
-        char buf[64];
-        sprintf(buf, "COOLING\n");
-        UART_PC_PutString(buf);
     } else {
+        state = STATE_IDLE;
         PIN_P_COOL_Write(0);
     }
 }
@@ -138,11 +147,6 @@ int main(void)
             temp_copper = rtd_volt_to_temp(ADC_TEMP_CountsTo_Volts(adc_RTD0));
         }
 
-        // Print out temperatures
-        char buf[64];
-        sprintf(buf, "RTD %.2f\n", temp_copper);
-        UART_PC_PutString(buf);
-
 #ifdef EZTUNE
         CapSense_CSD_TunerComm();
 #else
@@ -157,5 +161,10 @@ int main(void)
         // Heat or cool depending on remote touch state
         if (PIN_REMOTE_TOUCH_Read()) heater_controller(temp_copper);
         else cooling_controller();
+
+        // print out status message
+        char buf[64];
+        sprintf(buf, "%s | %03d duty | RTD %.2f\n", state_names[state], PWM_HEATER_ReadCompare(), temp_copper);
+        UART_PC_PutString(buf);
     }
 }
